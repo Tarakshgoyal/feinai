80d14a4ca2ad4ab77481b1215fab15496703e32f
Tarakshgoyal <taraksh9a33@gmail.com>
2025-07-08 14:00:34 +0530

    Update page.tsx

--- app/(app)/finance/page.tsx
+++ app/(app)/finance/page.tsx
@@ -1,21 +1,21 @@
-"use client";
+// "use client";
 
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Index from "@/components/fin/Index";
+// import { Toaster } from "@/components/ui/toaster";
+// import { Toaster as Sonner } from "@/components/ui/sonner";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import Index from "@/components/fin/Index";
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
-export default function Home() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <Index />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
+// export default function Home() {
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <TooltipProvider>
+//         <Toaster />
+//         <Sonner />
+//         <Index />
+//       </TooltipProvider>
+//     </QueryClientProvider>
+//   );
+// }
